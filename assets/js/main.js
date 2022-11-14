53bd3627193273cd1f26cc5a713998ffd685a00f
lollobianco <loreb2002@gmail.com>
2022-11-14 14:15:11 +0100

    Creati elementi lista in modo dinamico

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,23 @@ var app = new Vue(
 	  el: '#app',
 		data:{
 	    
+         todoList: [
+
+            {
+               text: 'Andare a fare la spesa',
+               done: false,
+            },
+            {
+               text: 'Andare a correre',
+               done: false,
+            },
+            {
+               text: 'Fare esercizio',
+               done: false,
+            },
+
+         ]
+
 	  },
 		methods:{
 			
